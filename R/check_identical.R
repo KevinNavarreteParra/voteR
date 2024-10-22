343baba87dc50b1a6bd8d84636f6420acafcd609
KevinNavarreteParra <navark4@unlv.nevada.edu>
2024-10-22 14:15:43 -0700

    finished function and documented it

--- R/check_identical.R
+++ R/check_identical.R
@@ -1,8 +1,13 @@
 #' Title: Check for identical rows in a data frame
 #'
+#' @description
+#' This function takes as an input tabular data with variables corresponding to ballot-level voting patterns and returns the number of identical rows in the data frame. The purpose is to identify the number of permutations for which there are $n$ number of duplicates.
+#'
+#'
 #' @param data An object of class data frame
 #' @param dup_num A numeric value indicating the number of identical rows to check for
 #' @param logical A character value indicating the logical operator to use. Must be one of 'greater', 'equal', or 'less'
+#' @param name A character value indicating the name of the column to create in the output data frame. Default is 'count'
 #'
 #' @return A numeric value indicating the number of identical rows in the data frame
 #' @export
@@ -10,7 +15,11 @@
 #' @importFrom dplyr group_by_all tally filter
 #'
 #' @examples
-check_identical <- function(data, dup_num, logical) {
+#' data <- data.frame(a = c(1, 2, 3, 4, 5, 1, 2, 3, 4, 5),
+#'                   b = c(1, 2, 3, 4, 5, 1, 2, 3, 4, 5))
+#'
+#' check_identical(data, 2, "greater")
+check_identical <- function(data, dup_num, logical, name = "count") {
 
   if (!is.data.frame(data)) {
     stop("data must be of class data.frame")
@@ -26,18 +35,18 @@ check_identical <- function(data, dup_num, logical) {
   if (logical == "greater") {
     output <- data %>%
       group_by_all() %>%
-      tally(name = "count") %>%
-      filter(count > dup_num)
+      tally(name = name) %>%
+      filter(name > dup_num)
   } else if (logical == "equal") {
     output <- data %>%
       group_by_all() %>%
-      tally(name = "count") %>%
-      filter(count == dup_num)
+      tally(name = name) %>%
+      filter(name == dup_num)
   } else if (logical == "less") {
     output <- data %>%
       group_by_all() %>%
-      tally(name = "count") %>%
-      filter(count < dup_num)
+      tally(name = name) %>%
+      filter(name < dup_num)
   } else {
     stop("logical must be one of 'greater', 'equal', or 'less'")
   }
